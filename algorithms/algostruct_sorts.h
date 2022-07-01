1e8f1bab4c9c8bf423901dc91ba01e17ec8c0e32
John McDonnell <john@renderlogic.com>
2022-07-01 15:31:24 -0400

    ALGOSTRUCT-5: added some basic code scaffolding files for the app.

---
+++ algorithms/algostruct_sorts.h
@@ -0,0 +1,12 @@
+/*
+ * Algostruct Sorting Algorithms
+ * 
+ * Include with this header is all the various sorting algorithms. As I develop this I may further decompose this into isolated algorithmically atomic functions per include.
+ *
+ * Author: John McDonnell - JJM - john@hire-john.com
+ */
+
+#ifndef ALGOSTRUCT_ALGO_SORTING
+#define ALGOSTRUCT_ALGO_SORTING
+
+#endif
---
+++ main.c
@@ -0,0 +1,11 @@
+/*
+ * Algostruct Main
+ *
+ * Main entry function for the Algostruct application. 
+ *
+ * Author: John McDonnell - JJM - john@hire-john.com
+ */
+
+int main(int argc, char** argv){
+	return EXIT_SUCCESS;
+}
