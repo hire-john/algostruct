a2b4bc46f947eb649e890f541e3841feb3eee174
John McDonnell <john@renderlogic.com>
2022-07-05 20:10:33 -0400

    ALGOSTRUCT-16: commited the new scaffolding files. I will be writing the algorthms individually in each on of these files as I get time.

---
+++ algorithms/sorting/as_selection_sort.h
@@ -0,0 +1,14 @@
+#ifndef ALGOSTRUCT_ALGO_SORTING_SELECTION
+#define ALGOSTRUCT_ALGO_SORTING_SELECTION
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+/*
+ * pointer to array & size of array, sort order
+ */
+void selection_sort(int*, int*, int);
+
+#endif
