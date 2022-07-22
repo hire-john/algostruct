780264d4734488e19a0439637f2b9653997f7287
John McDonnell <john@renderlogic.com>
2022-07-22 19:07:12 -0400

    ALGOSTRUCT-34: added insertion sort asc/desc conditional ordering.

--- algorithms/sorting/as_algo_insertion_sort.c
+++ algorithms/sorting/as_algo_insertion_sort.c
@@ -5,9 +5,16 @@ void insertion_sort(unsigned int* array, int* size, int sort_order){
 	for(int i = 1; i < *size; i++){
 		curr = array[i];
 		prev = i - 1;
-		while(prev >= 0 && array[prev] > curr){
-			array[prev + 1] = array[prev];
-			prev = prev - 1;
+		if(sort_order){
+			while(prev >= 0 && array[prev] > curr){
+				array[prev + 1] = array[prev];
+				prev = prev - 1;
+			}
+		}else{
+			while(prev >= 0 && array[prev] < curr){
+				array[prev + 1] = array[prev];
+				prev = prev - 1;
+			}
 		}
 		array[prev + 1] = curr;
 	}
